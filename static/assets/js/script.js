27a02859f010714ce4329cbd07b0d0c3ab28fb98
shuchitachawla2909 <shuchita.chawla.cd.cse23@itbhu.ac.in>
2024-12-24 16:48:37 +0530

    mobile-home,explore

--- static/assets/js/script.js
+++ static/assets/js/script.js
@@ -8,3 +8,35 @@
 // registerLink.addEventListener('click',()=>{
 //   logregBox.classList.remove('active');
 // });
+
+// SHOW MENU
+
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if(navToggle) {
+  navToggle.addEventListener('click', () => {
+    navMenu.classList.add('show-menu')
+  })
+}
+/*===== MENU HIDE =====*/
+/* Validate if constant exists */
+if(navClose) {
+  navClose.addEventListener('click', () => {
+    navMenu.classList.remove('show-menu')
+  })
+}
+/*===== REMOVE MENU MOBILE =====*/
+const navLink=document.querySelectorAll('.nav_link')
+const linkAction = () => {
+  const navMenu = document.getElementById('nav-menu')
+  // When we click on each nav_link, we remove the show-menu class
+  navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*===== MENU HIDE =====*/
+/*===== MENU HIDE =====*/
