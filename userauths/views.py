a6e207b38932f7e1ee743f8e50422c2d3fc05904
shuchitachawla2909 <shuchita.chawla.cd.cse23@itbhu.ac.in>
2024-12-24 11:47:26 +0530

    sign-up,sign-in

--- userauths/views.py
+++ userauths/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render,redirect
 from userauths.forms import UserRegisterForm
-from django.contrib.auth import login,authenticate
+from django.contrib.auth import login,authenticate,logout
 from django.contrib import messages
+from django.conf import settings
+
+User=settings.AUTH_USER_MODEL
 
 def register_view(request):
+  if request.user.is_authenticated:
+    messages.warning(request,f"You are already logged in.")
+    redirect(request,"rest:home")
+
   if request.method=="POST":
     form=UserRegisterForm(request.POST or None)
     if form.is_valid():
@@ -25,3 +32,39 @@ def register_view(request):
 
   }
   return render(request,"userauths/sign-up.html",context)
+
+
+def login_view(request):
+  if request.user.is_authenticated:
+    return redirect("rest:home")
+  
+  if request.method== "POST":
+    email=request.POST.get("email")
+    password=request.POST.get("password")
+
+    try:
+      user=User.objects.get(email=email)
+    except:
+      messages.warning(request,f"User with email {email} does not exist.")
+
+    user=authenticate(request,email=email, password=password)
+
+    if user is not None:
+      login(request,user)
+      messages.success(request,"You are Logged In.")
+      return redirect("rest:home")
+    else:
+      messages.warning(request,"Wrong password entered.")
+  
+  context={}
+  return render(request,"userauths/sign-in.html",context)
+
+
+def logout_view(request):
+    logout(request)
+    messages.success(request,"You have been logged out.")
+    
+    return redirect("userauths:sign-in")
+
+
+
